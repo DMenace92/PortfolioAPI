a55e9766c86524567b14d84b0d2132c2760417cb
DMenace92 <dennisenwiya@gmail.com>
2024-07-15 21:11:27 -0700

    fixed project creattion

--- server.js
+++ server.js
@@ -19,6 +19,13 @@ app.use(emailer);
 app.use(user);
 app.use(projectRouter);
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 // app.use(pizzaRouter)
 
 // const bodyParser = require('body-parser');
